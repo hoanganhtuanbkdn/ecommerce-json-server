9ab4fc94917bae40f750c5cfd9ec434f627b2420
Tuan PHA <tuanpha.it@gmail.com>
2024-03-23 21:14:34 +0700

    feat: update cors

--- api/server.js
+++ api/server.js
@@ -5,6 +5,12 @@ const fs = require('fs')
 const server = jsonServer.create()
 const bodyParser = require('body-parser')
 const jwt = require('jsonwebtoken')
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
 
 const router = jsonServer.router('./db.json')
 const userdb = JSON.parse(fs.readFileSync('./db.json', 'UTF-8'))
@@ -130,6 +136,7 @@ server.use(middlewares)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
 }))
+server.use(cors(corsOptions))
 server.use(router)
 server.listen(4000, () => {
     console.log('JSON Server is running')
